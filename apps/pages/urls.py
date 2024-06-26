93322512c31de88d70e4468e40b2755cc45d8079
Serhii Diachok <sergey@sdyachok.com.ua>
2024-06-26 22:25:36 +0300

    feat: add 'about us' page

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, about
 
 app_name = "pages"
 
 urlpatterns = [
+    path("about-us/", about, name="about"),
     path("", index, name="index"),
 ]
